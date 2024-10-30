b11b3737b968a3a087e5aead7244532a799aaef5
andrei_davidenko <davidenkav@gmail.com>
2024-10-30 18:35:49 +0500

    deleted wrong service

--- backend/user-service/src/main/java/com/example/userservice/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
