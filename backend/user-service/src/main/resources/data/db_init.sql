e35bb0c62b619233be4c6352a2a7eecb58e12c31
andrei_davidenko <davidenkav@gmail.com>
2025-02-15 19:40:29 +0500

    extended db_init.sql

--- backend/user-service/src/main/resources/data/db_init.sql
+++ backend/user-service/src/main/resources/data/db_init.sql
@@ -47,7 +47,8 @@ CREATE TABLE public.users
     account_non_expired BOOLEAN                        NOT NULL DEFAULT TRUE,
     account_non_locked  BOOLEAN                        NOT NULL DEFAULT TRUE,
     qr_code_image_url   TEXT,
-    qr_code_secret      VARCHAR(255)
+    qr_code_secret      VARCHAR(255),
+    last_login          DATE
 );
 ALTER TABLE public.users
     OWNER TO "user";
@@ -74,31 +75,33 @@ CREATE TABLE public.user_roles
 ALTER TABLE public.user_roles
     OWNER TO "user";
 
+create table public.login_history
+(
+    id           bigint       not null,
+    created_at   timestamp(6) not null,
+    created_by   bigint       not null,
+    reference_id varchar(255),
+    updated_at   timestamp(6) not null,
+    updated_by   bigint       not null,
+    ip           varchar(255),
+    login_time   timestamp(6),
+    success      boolean      not null,
+    user_agent   varchar(255),
+    user_id      bigint       not null
+);
+
+alter table public.login_history
+    owner to "user";
 
 CREATE SEQUENCE public.primary_key_seq START 1 INCREMENT 1 NO MINVALUE NO MAXVALUE CACHE 1;
 ALTER SEQUENCE public.primary_key_seq OWNER TO "user";
 
-INSERT INTO public.roles
-VALUES (1, 'ADMIN',
-        'user:create, user:read, user:update, document:create, document:read, document:update, document:delete',
-        '2024-01-29 22:10:47.925942', 0, 'system',
-        '2024-01-29 22:10:47.926642', 0);
-INSERT INTO public.roles
-VALUES (2, 'USER', 'document:create, document:read, document:update',
-        '2024-01-29 22:10:47.925942', 0, 'system',
-        '2024-01-29 22:10:47.926642', 0);
-
-INSERT INTO public.users
-(id, user_id, created_at, created_by, reference_id,
- updated_at, updated_by, email, first_name, last_name,
- login_attempts, phone, bio, image_url,
- enabled, mfa, account_non_expired, account_non_locked, qr_code_image_url, qr_code_secret)
-VALUES (0, '123e4567-e89b-12d3-a456-426614174000', '2024-01-29 22:10:47.925942', 0, 'system',
-        '2024-01-29 22:10:47.926642', 0, 'system@gmail.com', 'System', 'System',
-        0, '1234567890', 'This is not a user but the system itself',
-        'https://cdn-icons-png.flaticon.com/128/2911/2911833.png',
-        true, true, true, true, NULL, NULL);
-
+INSERT INTO public.roles (id, name, authorities, created_at, created_by, reference_id, updated_at, updated_by) VALUES (1, 'ADMIN','user:create, user:read, user:update, document:create, document:read, document:update, document:delete','2024-01-29 22:10:47.925942', 0, 'system','2024-01-29 22:10:47.926642', 0);
+INSERT INTO public.roles (id, name, authorities, created_at, created_by, reference_id, updated_at, updated_by) VALUES (2, 'USER', 'document:create, document:read, document:update', '2024-01-29 22:10:47.925942', 0, 'system', '2024-01-29 22:10:47.926642', 0);
+INSERT INTO public.users (id, user_id, created_at, created_by, reference_id, updated_at, updated_by, email, first_name, last_name, login_attempts, phone, bio, image_url, mfa, enabled, account_non_expired, account_non_locked, qr_code_image_url, qr_code_secret, last_login) VALUES (0, '123e4567-e89b-12d3-a456-426614174000', '2024-01-29 22:10:47.925942', 0, 'system', '2024-01-29 22:10:47.926642', 0, 'system@gmail.com', 'System', 'System', 0, '1234567890', 'This is not a user but the system itself', 'https://cdn-icons-png.flaticon.com/128/2911/2911833.png', true, true, true, true, null, null, '2025-02-15');
+INSERT INTO public.users (id, user_id, created_at, created_by, reference_id, updated_at, updated_by, email, first_name, last_name, login_attempts, phone, bio, image_url, mfa, enabled, account_non_expired, account_non_locked, qr_code_image_url, qr_code_secret, last_login) VALUES (1, 'f50ed85f-4ba4-4646-a459-811ea14a6313', '2025-02-15 15:58:10.778095', 0, '471f8c16-ec1c-21ff-3a5e-ee5d093fa551', '2025-02-15 15:58:28.164503', 0, 'admin@mail.com', 'admin', 'admin', 0, '', '', 'https://cdn-icons-png.flaticon.com/512/149/149071.png', false, true, true, true, null, '', '2025-02-15');
+INSERT INTO public.credentials (id, created_at, created_by, reference_id, updated_at, updated_by, password, user_id) VALUES (2, '2025-02-15 15:58:11.194212', 0, '5e13cd63-6264-03f2-6830-cf605127186c', '2025-02-15 15:58:11.194212', 0, '$2a$12$y2sbcrQ99J06DNX1M9PwruGS0RI1BTgp.4BanG1kHDhjBf9kHeL4i', 1);
+INSERT INTO public.user_roles (user_id, role_id) VALUES (1, 1);
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
@@ -116,3 +119,8 @@ ALTER TABLE public.user_roles
     ADD CONSTRAINT "FK7ov27fyo7ebsvada1ej7qkphl" FOREIGN KEY (user_id) REFERENCES public.users (id);
 ALTER TABLE public.user_roles
     ADD CONSTRAINT "FKej3jidxlte0r8flpavhiso3g6" FOREIGN KEY (role_id) REFERENCES public.roles (id);
+
+ALTER TABLE ONLY public.login_history
+    ADD CONSTRAINT login_history_pkey PRIMARY KEY (id);
+ALTER TABLE public.login_history
+    ADD CONSTRAINT "FKbuiuqeym9nh2ocv2kkyql36ne" FOREIGN KEY (user_id) REFERENCES public.users (id);
